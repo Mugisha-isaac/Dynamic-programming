6c5ee3771680144703d11b393086c573c6085e83
Mugisha-isaac <mugishaisaac2001@gmail>
2022-05-12 01:00:09 +0200

    refact(canConstruct.js) added memoization

--- canConstruct.js
+++ canConstruct.js
@@ -1,15 +1,20 @@
-const canConstruct = (target,wordbank)=>{
+const canConstruct = (target,wordbank, memo={})=>{
+   if(target in memo) return memo[target]; 
    if(target === '') return true;
    for(let word of wordbank){
        if(target.indexOf(word)===0){
             const suffix = target.slice(word.length);
-           if(canConstruct(suffix,wordbank)=== true){
-               return true;
+           if(canConstruct(suffix,wordbank, memo)=== true){
+            //    return true;
+            memo[target] = true;
+            return memo[target];
            }
        }
    }
 
-   return false;
+//    return false;
+memo[target] = false;
+return memo[target];
 }
 
 console.log(canConstruct("abcdef",["ab","abc","cd","def","abcd"]));
