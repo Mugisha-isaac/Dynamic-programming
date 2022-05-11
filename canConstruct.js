daf58428853aefefc7266a921ea69037bdc100a2
Mugisha-isaac <mugishaisaac2001@gmail>
2022-05-12 00:38:07 +0200

    chore(canConstruct.js) returns boolean

---
+++ canConstruct.js
@@ -0,0 +1,16 @@
+const canConstruct = (target,wordbank)=>{
+   if(target === '') return true;
+   for(let word of wordbank){
+       if(target.indexOf(word)===0){
+            const suffix = target.slice(word.length);
+           if(canConstruct(suffix,wordbank)=== true){
+               return true;
+           }
+       }
+   }
+
+   return false;
+}
+
+console.log(canConstruct("abcdef",["ab","abc","cd","def","abcd"]));
+console.log(canConstruct("skateboard",["bo","rd","ate","t","ska","sk","boar"]))
