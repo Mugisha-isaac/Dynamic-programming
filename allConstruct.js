499adafca708452613ab356ed1340247c6c05771
Mugisha-isaac <mugishaisaac2001@gmail>
2022-05-12 20:02:13 +0200

    refactor(allConstruct.js)  memoizing the algorithm

--- allConstruct.js
+++ allConstruct.js
@@ -1,20 +1,23 @@
 
 
-const allConstruct = (target,wordBank)=>{
+const allConstruct = (target,wordBank, memo={})=>{
+    if(target in memo) return memo[target]
    if(target === '') return [[]];
    const result = [];
    
    for(let word of wordBank){
        if(target.indexOf(word)===0){
            let suffix = target.slice(word.length);
-          let suffixWays = allConstruct(suffix,wordBank);
+          let suffixWays = allConstruct(suffix,wordBank,memo);
           let targetWays = suffixWays.map(way=>[word,...way]);
           result.push(...targetWays);
        }
    }
 
-   return result;
+   memo[target] = result;
+   return memo[target];
 }
 
-console.log(allConstruct("aaaaaaaaaaaaaaaaaaaaaaz",["a","aa","aaa","aaaa","aaaaa"]));
+
 console.log(allConstruct("purple",["purp","p","ur","le","purpl"]))
+console.log(allConstruct("aaaaaaaaaaaaaaaaaaaaaaz",["a","aa","aaa","aaaa","aaaaa"]));
