787d01355ffc11d23e2bdafd8979abf7cbdd114d
Mugisha-isaac <mugishaisaac2001@gmail>
2022-05-19 15:41:21 +0200

    chore(allConstructTabulation.js)  dynamic program

--- allConstructTabulation.js
+++ allConstructTabulation.js
@@ -1,12 +1,22 @@
 
 const allConstructTabulation = (target,wordBank)=>{
-    const table = Array(target.length +1).fill();
-    table[0] = [];
+    const table = Array(target.length +1).fill().map(()=>[]);
+    table[0] = [[]];
+    
+    for(let i=0;i<=target.length;i++){
+        for(let word of wordBank){
+            if(target.slice(i,i+word.length)===word){
+              const newCombinations =  table[i].map(subArray=>[...subArray,word]);
+              table[i+word.length].push(...newCombinations);
+            }
+        }
+    }
 
+    return table[target.length]
 }
 
 
-console.log(countConstructTabulation('purple',['purp','p','ur','le','purpl']));
-console.log(countConstructTabulation("abcdef",["ab","abc","cd","def","abcd"]));
-console.log(countConstructTabulation("skateboard",["bo","rd","ate","t","ska","sk","boar"]))
-console.log(countConstructTabulation("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef",["e","ee","eee","eeee","eeeee","eeeeee"]))
+console.log(allConstructTabulation('purple',['purp','p','ur','le','purpl']));
+// console.log(allConstructTabulation("abcdef",["ab","abc","cd","def","abcd"]));
+// console.log(allConstructTabulation("skateboard",["bo","rd","ate","t","ska","sk","boar"]))
+// console.log(allConstructTabulation("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef",["e","ee","eee","eeee","eeeee","eeeeee"]))
